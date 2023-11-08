053eae4621db9601cdb395429a03d83c1c410c0c
Ali Macrae <alasdair.macrae@jagex.com>
2023-11-08 13:29:57 +0000

    chore(main): update config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        formats: ["image/jpg", "image/webp"],
+        formats: ["image/avif", "image/webp"],
     }
 };
 
