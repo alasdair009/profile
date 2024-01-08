0e15398e72cac91baca0d2bbbb235dcd68753105
Ali Macrae <alasdair.macrae@jagex.com>
2024-01-08 13:05:28 +0000

    fix(main): fix manifest issues

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,12 +1,16 @@
 import { MetadataRoute } from "next";
 import { siteDescription, siteName } from "@/lib/metadata";
 import { colors } from "@/entities";
+import screenshotNarrow from "../entities/assets/screenshot-narrow.png";
+import screenshotWide from "../entities/assets/screenshot-wide.png";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: `${siteName}'s Portfolio`,
     short_name: siteName,
     description: siteDescription,
+    display_override: ["window-controls-overlay"],
+    id: "https://www.alasdairmacrae.co.uk",
     start_url: "/",
     display: "standalone",
     background_color: colors.blackEvil,
@@ -16,6 +20,24 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/icon.svg",
         sizes: "any",
         type: "image/svg+xml",
+      },
+    ],
+    screenshots: [
+      {
+        src: screenshotWide.src,
+        sizes: "1606x1216",
+        type: "image/png",
+        // @ts-ignore
+        form_factor: "wide",
+        label: "AM home",
+      },
+      {
+        src: screenshotNarrow.src,
+        sizes: "433x336",
+        type: "image/png",
+        // @ts-ignore
+        form_factor: "narrow",
+        label: "AM home",
       },
     ],
   };
