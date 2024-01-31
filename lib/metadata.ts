a237e6d43c2c61bf127a382ef189a7ec403ddd3b
Ali Macrae <alasdair.macrae@jagex.com>
2024-01-31 00:37:40 +0000

    fix(main): update to new url

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import { vercelOrigin } from "@/lib/domains";
+import { siteOrigin } from "@/lib/domains";
 
 export const siteName = "Alasdair Macrae";
 export const siteDescription = "The home of exciting web adventures";
@@ -12,17 +12,17 @@ export const generateMetaData = (
     title: `${pageTitle} | ${siteName}`,
     description: pageDescription,
     manifest: "/manifest.webmanifest",
-    metadataBase: new URL(`${vercelOrigin}/${pagePath}`),
+    metadataBase: new URL(`${siteOrigin}/${pagePath}`),
     openGraph: {
       title: `${pageTitle} | ${siteName}`,
-      images: `${vercelOrigin}/og?title=${pageTitle}`,
+      images: `${siteOrigin}/og?title=${pageTitle}`,
       description: pageDescription,
-      url: `https://profile-ivory-three.vercel.app/${pagePath}`,
+      url: `${siteOrigin}/${pagePath}`,
     },
     twitter: {
       site: "@alasdair009",
       card: "summary_large_image",
-      images: `${vercelOrigin}/og?title=${pageTitle}`,
+      images: `${siteOrigin}/og?title=${pageTitle}`,
       title: `${pageTitle} | ${siteName}`,
       description: pageDescription,
     },
