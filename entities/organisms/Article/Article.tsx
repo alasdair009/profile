f8734876f722397eca7a9f4e4d70ad78a68c7fd7
Ali Macrae <alasdair.macrae@jagex.com>
2024-07-04 18:56:15 +0100

    chore(main): more minor blog fixes

--- entities/organisms/Article/Article.tsx
+++ entities/organisms/Article/Article.tsx
@@ -16,6 +16,20 @@ type ArticleProps = {
   children: ReactNode;
 } & HTMLAttributes<HTMLDivElement>;
 
+const nthNumber = (number: number) => {
+  if (number > 3 && number < 21) return "th";
+  switch (number % 10) {
+    case 1:
+      return "st";
+    case 2:
+      return "nd";
+    case 3:
+      return "rd";
+    default:
+      return "th";
+  }
+};
+
 /**
  * Large content box with heading
  */
@@ -26,14 +40,18 @@ export function Article({
   children,
   ...rest
 }: ArticleProps) {
+  const dayNumber = date.toLocaleDateString("en-GB", {
+    day: "numeric",
+  });
   return (
     <Root {...rest}>
       <ArticleHeading align="center">{heading}</ArticleHeading>
       <ArticleDate dateTime={date.toISOString()}>
+        {dayNumber}
+        {nthNumber(parseInt(dayNumber))}&nbsp;
         {date.toLocaleDateString("en-GB", {
           year: "numeric",
-          month: "short",
-          day: "numeric",
+          month: "long",
         })}
       </ArticleDate>
       <Spacer multiplier={6} />
@@ -45,6 +63,7 @@ export function Article({
           style={{ objectFit: "cover" }}
         />
       </ArticleFigure>
+      <Spacer multiplier={8} />
       <Content>{children}</Content>
     </Root>
   );
