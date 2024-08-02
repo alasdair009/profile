42a009c16db10fae87786d1e5da290ba9f9f4f23
Ali Macrae <alasdair.macrae@jagex.com>
2024-08-02 16:31:53 +0100

    fix(main): bump storybook

--- entities/organisms/Header/Header.stories.tsx
+++ entities/organisms/Header/Header.stories.tsx
@@ -1,8 +1,12 @@
 import { Header } from "./Header";
 import { Meta, StoryObj } from "@storybook/react";
+import { ComponentType } from "react";
 
 const meta: Meta<typeof Header> = {
   component: Header,
+  subcomponents: {
+    NotificationButton: Header.NotificationButton as ComponentType<unknown>,
+  },
   parameters: {
     controls: { hideNoControlsWarning: true },
   },
