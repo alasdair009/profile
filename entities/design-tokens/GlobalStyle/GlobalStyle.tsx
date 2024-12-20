311d2260a279885c0cc63cc64afaf688db43315b
Ali Macrae <alasdair.macrae@jagex.com>
2024-12-20 15:08:21 +0000

    fix(main): custom scrollbar

--- entities/design-tokens/GlobalStyle/GlobalStyle.tsx
+++ entities/design-tokens/GlobalStyle/GlobalStyle.tsx
@@ -3,10 +3,13 @@ import { resets } from "./resets";
 import { Global } from "@storybook/theming";
 import { recaptcha } from "./recaptcha";
 import { motionStyles } from "../animations";
+import { scrollbar } from "../scrollbar";
 
 /**
  * Global style setup for JDS.
  */
 export function GlobalStyle(): JSX.Element {
-  return <Global styles={`${resets} ${recaptcha} ${motionStyles}`} />;
+  return (
+    <Global styles={`${resets} ${recaptcha} ${motionStyles} ${scrollbar}`} />
+  );
 }
---
+++ entities/design-tokens/scrollbar/index.ts
@@ -0,0 +1,1 @@
+export { scrollbar, scrollbarStyles } from "./scrollbar";
