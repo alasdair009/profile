a7f6cf4c8ee27fb27c8b5089cec99588c78ad40b
Ali Macrae <alasdair.macrae@jagex.com>
2025-03-04 23:33:00 +0000

    chore(main): remove commented code

--- app/portfolio/weather/page.tsx
+++ app/portfolio/weather/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
 import { generateMetaData } from "@/lib/metadata";
-import { Container, ErrorText, Heading, Paragraph } from "@/entities";
+import { Heading, Paragraph } from "@/entities";
 import { WeatherWrapper } from "@/app/portfolio/weather/components/WeatherWrapper";
 
 export const metadata: Metadata = generateMetaData(
@@ -10,46 +10,6 @@ export const metadata: Metadata = generateMetaData(
 );
 
 export default async function WeatherPage() {
-  // const query = `https://api.netatmo.com/api/getstationsdata?get_favorites=false`;
-  // let station,
-  //   allData: WeatherStationData,
-  //   indoorData: IndoorData | undefined,
-  //   outdoorData: OutdoorData | undefined,
-  //   rainData: RainData | undefined,
-  //   windData: WindData | undefined;
-  // const stationResponse = await fetch(query, {
-  //   headers: {
-  //     Authorization: `Bearer ${process.env.NETATMO_WEATHER_TOKEN}`,
-  //   },
-  // });
-  //
-  // if (stationResponse.ok) {
-  //   allData = await stationResponse.json();
-  //   station = allData.body.devices[0];
-  //
-  //   indoorData = station.dashboard_data;
-  //
-  //   outdoorData = station.modules.filter((obj) => {
-  //     return obj.module_name === "Outdoor Weather Station";
-  //   })[0].dashboard_data as OutdoorData;
-  //
-  //   rainData = station.modules.filter((obj) => {
-  //     return obj.module_name === "Rain Gauge";
-  //   })[0].dashboard_data as RainData;
-  //
-  //   windData = station.modules.filter((obj) => {
-  //     return obj.module_name === "Anemometer";
-  //   })[0].dashboard_data as WindData;
-  // } else {
-  //   console.log(
-  //     `Warning: Could not get response from Netatmo using token of: ${process.env.NETATMO_WEATHER_TOKEN}`
-  //   );
-  //
-  //   stationResponse.text().then((text) => {
-  //     throw new Error(text);
-  //   });
-  // }
-
   return (
     <>
       <Heading>Weather</Heading>
