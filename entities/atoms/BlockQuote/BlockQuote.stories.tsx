f5b82431fdfbee1ccf183b0de23c094fe38d0cf0
Ali Macrae <alasdair.macrae@jagex.com>
2025-12-10 19:23:29 +0000

    fix(main): some audit fixes

--- entities/atoms/BlockQuote/BlockQuote.stories.tsx
+++ entities/atoms/BlockQuote/BlockQuote.stories.tsx
@@ -13,9 +13,31 @@ export default meta;
 export const Default: StoryObj<typeof BlockQuote> = {
   play: async ({ args, canvasElement }) => {
     const canvas = within(canvasElement);
-    const blockQuoteElement = canvas.getByTestId(BlockQuote.name);
+    const blockQuoteElement = canvas.getByTestId(BlockQuote.displayName);
 
     await expect(blockQuoteElement).toBeInTheDocument();
     await expect(blockQuoteElement).toHaveTextContent(`${args.children}`);
+  },
+};
+
+export const WithCite: StoryObj<typeof BlockQuote> = {
+  args: {
+    citeTitle: "Alasdair Macrae",
+    citeUrl:
+      "https://design.alasdairmacrae.co.uk/?path=/story/atoms-blockquote--with-cite",
+  },
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement);
+    const blockQuoteElement = canvas.getByTestId(BlockQuote.displayName);
+    const citeLink = canvas.getByText(`${args.citeTitle}`);
+
+    await expect(blockQuoteElement).toBeInTheDocument();
+    await expect(blockQuoteElement).toHaveTextContent(`${args.children}`);
+    await expect(blockQuoteElement.children[0]).toHaveAttribute(
+      "cite",
+      args.citeUrl
+    );
+    await expect(citeLink).toBeInTheDocument();
+    await expect(citeLink).toHaveAttribute("href", args.citeUrl);
   },
 };
--- entities/atoms/BlockQuote/BlockQuote.tsx
+++ entities/atoms/BlockQuote/BlockQuote.tsx
@@ -1,26 +1,49 @@
 import { HTMLAttributes, ReactNode } from "react";
 import styles from "./BlockQuote.module.scss";
+import { Link } from "@/entities";
 
 type BlockQuoteProps = {
   /**
-   * The citation for the quote
+   * Citation source
    */
-  cite?: string;
+  citeUrl?: string;
+  /**
+   * Citation string to render.
+   */
+  citeTitle?: string;
   /**
    * Content of the quote
    */
   children: ReactNode;
-} & HTMLAttributes<HTMLSpanElement>;
+} & HTMLAttributes<HTMLDivElement>;
 
 /**
  * A large highlight of a short string.
  */
-export function BlockQuote({ ...rest }: BlockQuoteProps) {
+export function BlockQuote({
+  citeUrl,
+  citeTitle,
+  className = "",
+  children,
+  ...rest
+}: BlockQuoteProps) {
   return (
-    <blockquote
-      className={styles.root}
-      data-testid={BlockQuote.name}
+    <div
+      className={`${styles.root} ${className}`}
+      data-testid={BlockQuote.displayName}
       {...rest}
-    />
+    >
+      <blockquote cite={citeUrl} className={styles.blockQuote}>
+        {children}
+      </blockquote>
+      {citeUrl && citeTitle && (
+        <cite className={styles.cite}>
+          <Link className={styles.citeLink} href={citeUrl}>
+            {citeTitle}
+          </Link>
+        </cite>
+      )}
+    </div>
   );
 }
+BlockQuote.displayName = "BlockQuote";
