e5346e0d66cb5b2ea1573f2b96b6267fbde02b4d
Ali Macrae <alasdair.macrae@jagex.com>
2025-12-23 15:24:22 +0000

    fix(postcss): stylelint fixes

--- entities/design-tokens/clipPaths/clipPaths.ts
+++ entities/design-tokens/clipPaths/clipPaths.ts
@@ -1,5 +1,5 @@
 import { CSSProperties } from "react";
-import variables from "./ClipPaths.module.css";
+// import variables from "./ClipPaths.module.css";
 
 // export const clipPaths: Record<string, CSSProperties["clipPath"]> = JSON.parse(
 //   variables.clipPaths.replaceAll("'", "")
