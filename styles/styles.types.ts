09055e26925c066166b9e013320ecf92326a51d1
Ali Macrae <alasdair.macrae@jagex.com>
2026-08-05 15:33:29 +0100

    fix(main): bump to typescript 6

--- styles/styles.types.ts
+++ styles/styles.types.ts
@@ -8,22 +8,10 @@ export type HeadingTypes = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 export type Dimension = Exclude<CSSProperties["width"], number | undefined>;
 
 export type Breakpoint =
-  | "base"
-  | "min"
-  | "xsmall"
-  | "small"
-  | "medium"
-  | "large"
-  | "xlarge"
-  | "wide";
+  "base" | "min" | "xsmall" | "small" | "medium" | "large" | "xlarge" | "wide";
 
 export type FontSizes =
-  | "small"
-  | "medium"
-  | "mlarge"
-  | "large"
-  | "xlarge"
-  | "xxlarge";
+  "small" | "medium" | "mlarge" | "large" | "xlarge" | "xxlarge";
 
 export type TextAlignment = "left" | "center" | "right";
 
